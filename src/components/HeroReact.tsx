324727dcbeea14f8ebeb620211db58409301df0c
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-09-26 17:35:39 -0500

    first commit

--- src/components/HeroReact.tsx
+++ src/components/HeroReact.tsx
@@ -2,14 +2,14 @@ import { useEffect, useRef, useState } from 'react';
 //@ts-ignore
 import Typed from 'typed.js';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Swiper, SwiperSlide } from 'swiper/react';
 // import required modules
-import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+// import { EffectFade, Navigation, Pagination } from 'swiper/modules';
 
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import 'swiper/css';
+// import 'swiper/css/effect-fade';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 import { ImageCarrousel } from './ImageCarrousel';
 
 export const HeroReact = () => {
